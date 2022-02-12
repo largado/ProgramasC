0b3b7a08efd15738b082ff9391c19699fa674031
largado <49825687+largado@users.noreply.github.com>
2022-02-11 21:59:27 -0300

    primeiro commit
    11/02/2022

---
+++ 08.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+//conversão de inteiro para ponto flutuante
+
+int main(){
+    int a = 9;
+    int b = 7;
+    double flutuante = a / (double)b;
+    printf("ö valor é: %f\n" , flutuante);
+    
+}
---
+++ ex13.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//exercício 14
+
+int main () {
+    printf("entre com duas variáveis:\n");
+    int numero;
+    int variavel;
+    int resultado;
+
+    scanf("%d %d", &variavel, &numero);
+    resultado = variavel * numero;
+    printf("o resultado da multiplicação é: %d\n", resultado);
+
+
+
+
+    
+}
